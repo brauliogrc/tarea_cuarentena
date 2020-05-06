bec321de4f935ec6dd02ef393827706caa326349
brauliogrc <64605193+brauliogrc@users.noreply.github.com>
2020-05-06 13:18:18 -0500

    Actualización de código act9.py
    Modificación del ordenamiento de la lista.
    Se utilizo el método de "Bubble Sort" para el ordenamiento de la listas en lugar del método "lista.sort()" de manejo de Listas

--- act9.py
+++ act9.py
@@ -4,7 +4,14 @@ while(i<10):
     numero=int(input("Ingrese numero " + str(i+1) + ": "))
     lista.append(numero)
     i+=1
-lista.sort()
+
+for i in range(len(lista)-1, 0, -1):
+    for j in range(i):
+        if (lista[j] > lista[j+1]):
+            aux = lista[j]
+            lista[j] = lista[j+1]
+            lista[j+1] = aux
+
+#lista.sort()
 print(lista)
 print(max(lista))
-
